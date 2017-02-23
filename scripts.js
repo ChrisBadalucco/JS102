be7e2f7f008ec66e3e8d9340f4a89f7e26d04b08
Chris Badalucco <cbadalucco@gmail.com>
2017-02-22 22:55:51 -0500

    exercise 1 and 2

--- scripts.js
+++ scripts.js
@@ -1,3 +1,40 @@
-//Use this file to implement Part One of your project
+// part 1 exercise
+var animal = {};
+animal.username = 'Pooh';
+animal['tagline'] = 'Oh bother';
 
+var noises = [];
+animal.noises = noises;
 
+var count = 0;
+for (var key in animal) {
+    count ++;
+    if (key === 'username') {
+        console.log('Hi my name is ' + animal[key]);
+    } else if (key === 'tagline') {
+        console.log('I like to say ' + animal[key]);
+    }
+}
+
+// part 2 exercise - arrays
+var noiseArray = ['honk'];
+
+noiseArray.unshift('beep');
+noiseArray.push('moo');
+
+noiseArray[noiseArray.length] = 'bahh';
+
+animal.noises = noiseArray;
+
+// part 2 exercise - collections
+var animals = [];
+animals.push(animal);
+
+var quackers = { username: 'DaffyDuck', tagline: 'Yippeee!', noises: ['quack', 'honk', 'sneeze', 'growl'] };
+
+animals.unshift(quackers);
+
+var snek = { username: 'Snek', tagline: 'Slitherin', noises: ['hiss', 'chirp', 'growl'] };
+var thumper = { username: 'Thumper', tagline: 'Thumpin', noises: ['thump', 'gnaw', 'meh'] };
+
+animals.push(snek, thumper);
